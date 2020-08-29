0e200be82ed843dbd21229d0e0d879957a9cd159
Ryan Flores <ryanzacharyflores@gmail.com>
2020-08-29 01:46:44 -0700

    Cleaned up edit-essay code, added starter for highlighted-text

--- src/components/edit-essay.component.js
+++ src/components/edit-essay.component.js
@@ -1,51 +1,103 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
+import HighlightedText from "./highlighted-text.component";
 
 export default class EditEssay extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            essay: [],
-            test: 'test1 test1 test1 test1'
-        };
-
         this.handleMouseUp = this.handleMouseUp.bind(this);
+        this.handleSelection = this.handleSelection.bind(this);
+        this.onChangeMessage = this.onChangeMessage.bind(this);
+
+        this.state = {
+            username: '',
+            description: '',
+            content: '',
+            date: new Date(),
+            message: ''
+        }
+
+        axios.get('http://localhost:5000/essays/'+this.props.match.params.id)
+            .then(response => {
+                console.log(response.data.username);
+                this.setState({
+                    username: response.data.username,
+                    description: response.data.description,
+                    content: response.data.content,
+                    date: new Date(response.data.date)
+                })
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
 
     componentDidMount() {
         window.addEventListener('mouseup', this.handleMouseUp, true);
     }
 
-    handleMouseUp(e) {
-        console.log("here!");
-        console.log(this.state.test);
+    handleMouseUp(ready) {
+        // Delay necessary to prevent accidental trigger upon deselection
+        setTimeout(this.handleSelection, 50);
+    }
+
+    onChangeMessage(e) {
+        this.setState({
+            message: e.target.value
+        })
+    }
+
+    handleSelection() {
+        const content = document.getElementById("content");
 
         let selection = window.getSelection();
         let cursorStart = selection.anchorOffset;
         let cursorEnd = selection.focusOffset;
-
-        const content = document.getElementById("content");
-        const found = selection.containsNode(content, true);
-
-        console.log('found: ' + found);
-
         let textStart = Math.min(cursorStart, cursorEnd);
         let textEnd = Math.max(cursorStart, cursorEnd);
 
-        let text = this.state.test.substring(textStart, textEnd);
+        // Check that essay content is being selected and selection size != 0
+        const found = selection.containsNode(content, true) && !(textStart === textEnd);
 
-        console.log(cursorStart);
-        console.log(cursorEnd);
-        console.log(text);
+        if (found) {
+            let text = this.state.content.substring(textStart, textEnd);
+
+            console.log("start: " + textStart + " end: " + textEnd + " text: " + text);
+        }
     }
 
     render() {
         return (
-            <div id="content">
+            <Container>
                 This is the edit essay component
-            </div>
+                <br/>
+
+                <Row>
+                    <Col>
+                        <label>Content: </label>
+                        <br/>
+                        <p
+                            id="content"
+                        >
+                            {this.state.content}
+                        </p>
+                        <HighlightedText />
+                    </Col>
+                    <Col>
+                        <div className="form-group">
+                            <label>Message: </label>
+                            <textarea
+                                required
+                                className="form-control"
+                                value={this.state.message}
+                                onChange={this.onChangeMessage}
+                            />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
---
+++ src/components/highlighted-text.component.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class HighlightedText extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                Yep.
+            </div>
+        );
+    }
+}
